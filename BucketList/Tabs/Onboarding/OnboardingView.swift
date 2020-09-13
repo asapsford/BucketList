ab477bcabb8f9e04f8d0b990ee25d58d32f862a3
Alex Sapsford <asapsford11@gmail.com>
2020-09-13 11:08:22 +0100

    change root view to onboarding view

--- BucketList/Tabs/Onboarding/OnboardingView.swift
+++ BucketList/Tabs/Onboarding/OnboardingView.swift
@@ -14,13 +14,11 @@ struct OnboardingView: View {
     @State private var isUnlocked = false
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Welcome to the worst welcoming page")
             
             // need to navigate to the bucket list tab view once click the button which will do the action of authenticating via biometrics
         }
     }
-
-
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
